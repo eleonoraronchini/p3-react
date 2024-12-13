35b2985730d56b881a51224508eb6e40160e1bcd
eleonoraronchini <eleonora.ronchini@outlook.it>
2024-12-13 18:04:20 +0100

    updates player e like/unlinke buttons

--- src/components/MusicSection.jsx
+++ src/components/MusicSection.jsx
@@ -16,7 +16,7 @@ const MusicSection = ({ title, data }) => {
 
   return (
     <div className="col-10">
-      <h2>{title}</h2>
+      <h2 style={{ color: 'white' }}>{title}</h2> 
       <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3 music-section">
         {data.map((song) => (
           <div key={song.id} className="col text-center">
@@ -26,11 +26,51 @@ const MusicSection = ({ title, data }) => {
               alt={song.title}
               onClick={() => handleSongClick(song)}
             />
-            <p>
-              Track: "{song.title}"<br />
-              Artist: {song.artist?.name}
-              <button onClick={() => handleLikeClick(song)}>
-                {likedSongs.some((likedSong) => likedSong.id === song.id) ? 'Unlike' : 'Like'}
+            <p style={{ color: 'white' }}> 
+               "{song.title}"<br />
+                {song.artist?.name}
+              <button
+                onClick={() => handleLikeClick(song)}
+                style={{
+                  background: 'none',
+                  border: 'none',
+                  cursor: 'pointer',
+                  display: 'inline-flex',
+                  alignItems: 'center', 
+                  justifyContent: 'center', 
+                }}
+              >
+                {likedSongs.some((likedSong) => likedSong.id === song.id) ? (
+                  // Icona cuore pieno per "like"
+                <div className='px-2'>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="white"
+                    className="bi bi-heart-fill"
+                    viewBox="0 0 16 16"
+                    style={{ marginRight: '8px' }} 
+                  >
+                    <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+                  </svg>
+                  </div>
+                ) : (
+                  // Icona cuore vuoto per "unlike"
+                  <div className='px-2'>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="white"
+                    className="bi bi-heart"
+                    viewBox="0 0 16 16"
+                    style={{ marginRight: '8px' }} 
+                  >
+                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                  </svg>
+                  </div>
+                )}
               </button>
             </p>
           </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -65,10 +65,6 @@ const Player = () => {
     }
   }, [audio]);
 
-  if (!currentSong) {
-    return <div className="container-fluid fixed-bottom bg-container pt-1">No song selected</div>;
-  }
-
   return (
     <div className="container-fluid fixed-bottom bg-container pt-2">
       <div className="row h-100">
@@ -76,39 +72,60 @@ const Player = () => {
           <div className="row h-100 flex-column justify-content-center align-items-center">
             <div className="col-8 col-md-6 playerControls">
               <div className="d-flex justify-content-between align-items-center">
-                <a href="#" className="player-button">
-                  <img src="/assets/shuffle.png" alt="shuffle" />
+                {/* Shuffle Button */}
+                <a href="#" className="player-button mx-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" className="bi bi-shuffle" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M0 3.5A.5.5 0 0 1 .5 3H1c2.202 0 3.827 1.24 4.874 2.418.49.552.865 1.102 1.126 1.532.26-.43.636-.98 1.126-1.532C9.173 4.24 10.798 3 13 3v1c-1.798 0-3.173 1.01-4.126 2.082A9.6 9.6 0 0 0 7.556 8a9.6 9.6 0 0 0 1.317 1.918C9.828 10.99 11.204 12 13 12v1c-2.202 0-3.827-1.24-4.874-2.418A10.6 10.6 0 0 1 7 9.05c-.26.43-.636.98-1.126 1.532C4.827 11.76 3.202 13 1 13H.5a.5.5 0 0 1 0-1H1c1.798 0 3.173-1.01 4.126-2.082A9.6 9.6 0 0 0 6.444 8a9.6 9.6 0 0 0-1.317-1.918C4.172 5.01 2.796 4 1 4H.5a.5.5 0 0 1-.5-.5"/>
+                    <path d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192"/>
+                  </svg>
                 </a>
-                <a href="#" onClick={handlePrevSong} className="player-button">
-                  <img src="/assets/prev.png" alt="prev" />
+
+                {/* Previous Button */}
+                <a href="#" onClick={handlePrevSong} className="player-button mx-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" className="bi bi-rewind-circle-fill" viewBox="0 0 16 16">
+                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M7.729 5.055A.5.5 0 0 1 8 5.5v1.886l3.21-2.293A.5.5 0 0 1 12 5.5v5a.5.5 0 0 1-.79.407L8 8.614V10.5a.5.5 0 0 1-.79.407l-3.5-2.5a.5.5 0 0 1 0-.814l3.5-2.5a.5.5 0 0 1 .519-.038"/>
+                  </svg>
                 </a>
-                <a href="#" onClick={handlePlayPause} className="player-button">
-                  <img src={isPlaying ? "/assets/pause.png" : "/assets/play.png"} alt="play-pause" />
+
+                {/* Play/Pause Button */}
+                <a href="#" onClick={handlePlayPause} className="player-button mx-3">
+                  {isPlaying ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" className="bi bi-pause-circle-fill" viewBox="0 0 16 16">
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5m3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5"/>
+                    </svg>
+                  ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" className="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z"/>
+                    </svg>
+                  )}
                 </a>
-                <a href="#" onClick={handleNextSong} className="player-button">
-                  <img src="/assets/next.png" alt="next" />
+
+                {/* Next Button */}
+                <a href="#" onClick={handleNextSong} className="player-button mx-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" className="bi bi-fast-forward-circle-fill" viewBox="0 0 16 16">
+                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M4.79 5.093 8 7.386V5.5a.5.5 0 0 1 .79-.407l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 8 10.5V8.614l-3.21 2.293A.5.5 0 0 1 4 10.5v-5a.5.5 0 0 1 .79-.407"/>
+                  </svg>
                 </a>
-                <a href="#" className="player-button">
-                  <img src="/assets/repeat.png" alt="repeat" />
+
+                {/* Repeat Button */}
+                <a href="#" className="player-button mx-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" className="bi bi-repeat" viewBox="0 0 16 16">
+                    <path d="M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z"/>
+                  </svg>
                 </a>
               </div>
-              <div className="progress mt-3" ref={progressBarRef} onClick={handleProgressChange}>
-                <div 
-                  role="progressbar" 
-                  aria-valuenow={progress} 
-                  aria-valuemin="0" 
-                  aria-valuemax="100" 
-                  style={{ width: `${progress}%` }}
-                  className="progress-bar-custom"
-                ></div>
-              </div>
+            </div>
+            {/* Song Progress */}
+            <div
+              className="progress"
+              style={{ cursor: 'pointer' }}
+              onClick={handleProgressChange}
+              ref={progressBarRef}
+            >
+              <div className="progress-bar" style={{ width: `${progress}%` }}></div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="song-info text-center">
-        <h4>{currentSong.title}</h4>
-        <p>{currentSong.artist?.name}</p>
       </div>
     </div>
   );
